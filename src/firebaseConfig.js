1f9ffa41cb0f142439450fa6459ed196280edf58
Faraz <ahmadfaraz541@gmail.com>
2023-04-15 16:48:40 +0500

    Added .env file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,12 +8,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBubPWFQu2Rz3Iew-ZyDuxz2kd53NnwhYA",
-  authDomain: "todo-app-8692c.firebaseapp.com",
-  projectId: "todo-app-8692c",
-  storageBucket: "todo-app-8692c.appspot.com",
-  messagingSenderId: "255543805908",
-  appId: "1:255543805908:web:33c3bfb31030ea0a927841",
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
